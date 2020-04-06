6112b2695f3d2f5503489f33c42a42fcfcd757cb
Newhart <newhartk@bouldercolorado.gov>
2020-04-06 08:50:25 -0600

    2020-04-06 data update 2

--- R output/Ammonia Forecast v2 - 0.R
+++ R output/Ammonia Forecast v2 - 0.R
@@ -4,4 +4,4 @@ predictor.variable <- "NIA33391_AB3_AMMONIA"
 predictor.tag <- "NIA33391_AB3_PREDICTED_AMMONIA.F_CV"
 forecast.horizon <- 50 # In minutes
 data.interval <- 60 # In seconds
-training.window <- 3+(forecast.horizon+5)/60/60 # in days, additional 5 min added for compiling time
+training.window <- 4+(forecast.horizon+5)/60/60 # in days, additional 5 min added for compiling time
