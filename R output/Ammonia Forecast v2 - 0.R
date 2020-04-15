c1b672e3de4ef5888acf8363360b459acac7a9c7
Newhart <newhartk@bouldercolorado.gov>
2020-04-14 20:59:14 -0600

    daily update

--- R output/Ammonia Forecast v2 - 0.R
+++ R output/Ammonia Forecast v2 - 0.R
@@ -3,5 +3,5 @@ historian.import.path <- paste0(getwd(),"/R output/")
 predictor.variable <- "NIA33391_AB3_AMMONIA"
 predictor.tag <- "NIA33391_AB3_PREDICTED_AMMONIA.F_CV"
 forecast.horizon <- 50 # In minutes
-data.interval <- 2*60 # In seconds
+data.interval <- 5*60 # In seconds
 training.window <- 4+(forecast.horizon+5)/60/60 # in days, additional 5 min added for compiling time
--- Raw data/Historian Data Export CSV v2.vbs
+++ Raw data/Historian Data Export CSV v2.vbs
@@ -1,4 +1,4 @@
-Const n = 0.00271934552325143
+Const n = 0.00139971573043753
 
 Call ExportDataToCSV("ww-historian", "WWSCADA2.BASIN3_16_DO_PVENG.F_CV", Now-n, Now)
 Call ExportDataToCSV("ww-historian", "WWSCADA2.BASIN3_7_DO_PVENG.F_CV", Now-n, Now)
